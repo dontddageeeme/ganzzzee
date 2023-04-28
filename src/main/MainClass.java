d6a95243b93cd09b574b9538c66b22b9e43041ab
goun <gouon123@naver.com>
2023-04-28 13:52:32 +0900

    오수

---
+++ src/main/MainClass.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	System.out.println("선택");
+	int num = sc.nextInt();
+	switch(num) {
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	}
+}
+}
